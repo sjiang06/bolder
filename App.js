c8e42434239fc0d5df84c1da7b8c0a32a9b8de64
Sofía Dudas <sdudas@stanford.edu>
2018-11-30 12:05:09 -0800

    added file`

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import {Button, View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { SQLite } from 'expo';
+import styled from 'styled-components/native';
+import {Button, View, Text, StyleSheet, Image, ScrollView, TouchableOpacity} from 'react-native';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
+
+const db = SQLite.openDatabase('db.db');
 
 class HomeScreen extends React.Component {
   render() {
     const resizeMode = 'cover';
-    const text = 'I am some centered text'
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1}}>
         <View
           style={{
             position: 'absolute',
@@ -28,11 +31,8 @@ class HomeScreen extends React.Component {
         <View style={styles.container}>
         
         <TouchableOpacity onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            this.props.navigation.navigate('Details', {
-              itemId: 86,
-              otherParam: 'anything you want here',
-            });
+            /* 1. Navigate to the Recordings route with params */
+            this.props.navigation.navigate('Recordings');
           }}>
           <View>
             <Image style={styles.buttonLeft} source={{uri: 'https://i.ibb.co/6D7f4kq/blue-blob.png'}} 
@@ -41,6 +41,7 @@ class HomeScreen extends React.Component {
             <Image style={styles.iconLeft} source={{uri: 'https://i.ibb.co/RHhhSgB/noun-Playlist-972339.png'}} 
               resizeMode="contain"
             />
+            <Text style={styles.textRight}> RECORDINGS </Text>
           </View>
         </TouchableOpacity>
 
@@ -52,16 +53,16 @@ class HomeScreen extends React.Component {
             <Image style={styles.iconRight} source={{uri: 'https://i.ibb.co/SJzVtRf/achievements.png'}} 
               resizeMode="contain"
             />
+            <Text style={styles.textLeft}> ACHIEVEMENTS </Text>
           </View>
         </TouchableOpacity>
 
       </View>
-        <Text>Home Screen</Text>
         <Button
-          title="Go to Details"
+          title="Go to Recordings"
           onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            this.props.navigation.navigate('Details', {
+            /* 1. Navigate to the Recordings route with params */
+            this.props.navigation.navigate('Recordings', {
               itemId: 86,
               otherParam: 'anything you want here',
             });
@@ -72,34 +73,61 @@ class HomeScreen extends React.Component {
   }
 }
 
-class DetailsScreen extends React.Component {
+const ImageContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+`;
+
+class RecordingsScreen extends React.Component {
   render() {
     /* 2. Get the param, provide a fallback value if not available */
-    const { navigation } = this.props;
-    const itemId = navigation.getParam('itemId', 'NO-ID');
-    const otherParam = navigation.getParam('otherParam', 'some default value');
+
 
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>itemId: {JSON.stringify(itemId)}</Text>
-        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() =>
-            this.props.navigation.push('Details', {
-              itemId: Math.floor(Math.random() * 100),
-            })}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
+      <View style={{paddingLeft: 30}}>
+        <Text style={{fontSize: 20, color: '#00B2CA', fontFamily: 'Gill Sans'}}>RECENT</Text>
+        <TouchableOpacity>
+          <View style={{flexDirection:'row'}}>
+            <Image style={{width: 130, height: 130}} source={{uri: 'https://i.ibb.co/D4Hwg4L/Screen-Shot-2018-11-30-at-2-33-39-AM.png'}}/>                  
+                <View style={{flexDirection:'column'}}>
+                    <Text style={{fontSize:20, color: '#000000', fontFamily: 'Gill Sans'}}> {MEMORIES[0].skill} </Text>
+                    <Text style={{fontSize:20, color: '#FBD1A2', paddingVertical: 10,fontFamily: 'Gill Sans'}}> {"November "}{MEMORIES[0].date} </Text>
+                    <Text style={{fontSize:16, color: '#FE938C', paddingVertical: 10, fontFamily: 'Gill Sans'}}> {MEMORIES[0].goals[0]} </Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+        <Text style={{fontSize: 20, color: '#00B2CA', fontFamily: 'Gill Sans'}}>SHOW: All</Text>
+        <ScrollView vertical={true} fillViewPort="true" contentContainerStyle={styles.scrollstyle}>
+          <ImageContainer>
+            {MEMORIES.map((memory, index) => (
+              <TouchableOpacity 
+                onPress={() => this.props.navigation.navigate('RecordingDetails', memory)}
+                key={index}>
+                <View style={{flexDirection:'row'}}>
+                  <Image style={styles.iconSmall} source={{uri: 'https://i.ibb.co/D4Hwg4L/Screen-Shot-2018-11-30-at-2-33-39-AM.png'}} key ={index} />
+                  <View style={{flexDirection:'column'}}>
+                    <Text style={{fontSize:20, color: '#FBD1A2', fontFamily: 'Gill Sans'}}> {"November "}{memory.date} </Text>
+                    <Text style={{fontSize:16, color: '#FBD1A2', paddingVertical: 0, fontFamily: 'Gill Sans'}}> {memory.skill} </Text>
+                    <Text style={{fontSize:16, color: '#FE938C', paddingVertical: 0, fontFamily: 'Gill Sans'}}> {memory.goals[0]} </Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </ImageContainer>
+        </ScrollView>
       </View>
+    );
+  }
+}
+
+class RecordingDetailsScreen extends React.Component {
+  render() {
+    const { memory } = this.props.navigation.state;
+    return (
+      <View>
+      <Text> Memory Details View </Text>
+      </View>
+
     );
   }
 }
@@ -146,13 +174,91 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: -30,
     left: -150
+  },
+  textLeft: {
+    position: 'absolute',
+    top: 110,
+    right: -180,
+    fontFamily: 'Gill Sans',
+    fontSize: 16,
+    color: 'white'
+  },
+  textRight: {
+    position: 'absolute',
+    top: 110,
+    left: -180,
+    fontFamily: 'Gill Sans',
+    fontSize: 16,
+    color: 'white'
+
+  },
+  iconSmall: {
+    width: 100,
+    height: 100,
+  },
+  scrollstyle: {
+    marginBottom:30
+  },
+  font: {
+    fontFamily: 'Gill Sans'
   }
 });
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+var SKILLS = ["Public Speaking", "Making New Friends", "Talking to Crush"];
+var PublicSpeakingGoals = ["Self Confidence", "Pacing", "Memorization", "Stuttering"];
+var MakingNewFriendsGoals = ["Asking Questions", "Introducing Self", "Initiating Hangouts", "Being Vulnerable"];
+var TalkingToCrushGoals = ["Texting first", "Planning hangouts", "Physical Affection", "Asking Questions"];
+const goalMap = new Map();
+goalMap.set(SKILLS[0], PublicSpeakingGoals);
+goalMap.set(SKILLS[1], MakingNewFriendsGoals);
+goalMap.set(SKILLS[2], TalkingToCrushGoals);
+
+const skillMap = new Map();
+var skill0Arr = [];
+skillMap.set(SKILLS[0], skill0Arr);
+var skill1Arr = [];
+skillMap.set(SKILLS[1], skill1Arr);
+var skill2Arr = [];
+skillMap.set(SKILLS[2], skill2Arr);
+
+var MEMORIES = [];
+var count;
+
+for(count = 0; count < 10; count++) {
+  var skillArr = [];
+  var skillChosen = SKILLS[getRandomInt(goalMap.size)];
+  var goalsArr = [];
+  var skillArr = goalMap.get(skillChosen);
+  var i;
+  for(i = 0; i < 2; i++){
+    goalsArr.push(skillArr[getRandomInt(skillArr.length)]);
+  };
+  var mem = {
+    index: count,
+    title: "PENDING",
+    date: getRandomInt(31),
+    skill: skillChosen,
+    goals: goalsArr,
+  };
+  skillMap.get(mem.skill).push(mem.index);
+  MEMORIES.push(mem);
+};
+
+function compareMemories(a, b){
+  return b.date>a.date
+}
+
+MEMORIES.sort(compareMemories)
 
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen,
+    Recordings: RecordingsScreen,
+    RecordingDetails: RecordingDetailsScreen,
   },
   {
     initialRouteName: 'Home',
