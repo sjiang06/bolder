8355c8c597558b2ac669e733f53a8a25d21fef5d
sjiang06 <yuchenj@stanford.edu>
2018-12-03 12:27:38 -0800

    added node module

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { SQLite } from 'expo';
 import styled from 'styled-components/native';
-import {Button, View, Text, StyleSheet, Image, ScrollView, TouchableOpacity} from 'react-native';
+import {Button, View, 
+  Text, StyleSheet, 
+  Image, ScrollView, 
+  TouchableOpacity, PanResponder,
+  Animated} from 'react-native';
+import GridView from 'react-native-gridview'
 import {createStackNavigator, createAppContainer} from 'react-navigation';
-
-const db = SQLite.openDatabase('db.db');
 
 class HomeScreen extends React.Component {
   render() {
@@ -28,12 +31,10 @@ class HomeScreen extends React.Component {
             source={{uri: 'https://i.ibb.co/kyynhkK/home.png'}}
           />
         </View>
-
-        <View style={styles.logo}>
+         <View style={styles.logo}>
           <Image source={require('./images/bolder_logo.png')} 
           style={{width: 400, height: 200}}/>
         </View>
-
         <View style={styles.container}>
         
         <TouchableOpacity onPress={() => {
@@ -84,6 +85,7 @@ const ImageContainer = styled.View`
   flex-direction: column;
 `;
 
+
 class RecordingsScreen extends React.Component {
   render() {
     /* 2. Get the param, provide a fallback value if not available */
@@ -98,11 +100,11 @@ class RecordingsScreen extends React.Component {
                 <View style={{flexDirection:'column'}}>
                     <Text style={{fontSize:20, color: '#000000', fontFamily: 'Gill Sans'}}> {MEMORIES[0].skill} </Text>
                     <Text style={{fontSize:20, color: '#FBD1A2', paddingVertical: 10,fontFamily: 'Gill Sans'}}> {"November "}{MEMORIES[0].date} </Text>
-                    <Text style={{fontSize:16, color: '#FE938C', paddingVertical: 10, fontFamily: 'Gill Sans'}}> {MEMORIES[0].goals[0]} </Text>
+                    <Text style={{fontSize:16, color: '#FE938C', paddingVertical: 10, fontFamily: 'Gill Sans'}}> {MEMORIES[0].goals[0].goalTitle} </Text>
                 </View>
             </View>
         </TouchableOpacity>
-        <Text style={{fontSize: 20, color: '#00B2CA', fontFamily: 'Gill Sans'}}>SHOW: All</Text>
+        <Text style={{fontSize: 20, color: '#00B2CA', fontFamily: 'Gill Sans'}}>SHOW: </Text>
         <ScrollView vertical={true} fillViewPort="true" contentContainerStyle={styles.scrollstyle}>
           <ImageContainer>
             {MEMORIES.map((memory, index) => (
@@ -112,9 +114,9 @@ class RecordingsScreen extends React.Component {
                 <View style={{flexDirection:'row'}}>
                   <Image style={styles.iconSmall} source={{uri: 'https://i.ibb.co/D4Hwg4L/Screen-Shot-2018-11-30-at-2-33-39-AM.png'}} key ={index} />
                   <View style={{flexDirection:'column'}}>
-                    <Text style={{fontSize:20, color: '#FBD1A2', fontFamily: 'Gill Sans'}}> {"November "}{memory.date} </Text>
+                    <Text style={{fontSize:20, color: '#646363', fontFamily: 'Gill Sans'}}> {"November "}{memory.date} </Text>
                     <Text style={{fontSize:16, color: '#FBD1A2', paddingVertical: 0, fontFamily: 'Gill Sans'}}> {memory.skill} </Text>
-                    <Text style={{fontSize:16, color: '#FE938C', paddingVertical: 0, fontFamily: 'Gill Sans'}}> {memory.goals[0]} </Text>
+                    <Text style={{fontSize:16, color: '#FE938C', paddingVertical: 0, fontFamily: 'Gill Sans'}}> {memory.goals[0].goalTitle} </Text>
                   </View>
                 </View>
               </TouchableOpacity>
@@ -126,20 +128,114 @@ class RecordingsScreen extends React.Component {
   }
 }
 
+class Draggable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showDraggable: true,
+      dropAreaValues: null,
+      pan: new Animated.ValueXY(),
+      opacity: new Animated.Value(1)
+    };
+  }
+
+  componentWillMount() {
+    this._val = { x:0, y:0 }
+    this.state.pan.addListener((value) => this._val = value);
+
+    this.panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (e, gesture) => true,
+        onPanResponderGrant: (e, gesture) => {
+          this.state.pan.setOffset({
+            x: this._val.x,
+            y:this._val.y
+          })
+          this.state.pan.setValue({ x:0, y:0})
+        },
+        onPanResponderMove: Animated.event([ 
+          null, { dx: this.state.pan.x, dy: this.state.pan.y }
+        ]),
+        onPanResponderRelease: (e, gesture) => {
+          if (this.isDropArea(gesture)) {
+            Animated.timing(this.state.opacity, {
+              toValue: 0,
+              duration: 1000
+            }).start(() =>
+              this.setState({
+                showDraggable: false
+              })
+            );
+          } 
+        }
+      });
+  }
+
+  isDropArea(gesture) {
+    return gesture.moveY < 200;
+  }
+
+  render() {
+    return (
+      <View style={{ width: "20%", alignItems: "center" }}>
+        {this.renderDraggable()}
+      </View>
+    );
+  }
+
+  renderDraggable() {
+    const panStyle = {
+      transform: this.state.pan.getTranslateTransform()
+    }
+    if (this.state.showDraggable) {
+      return (
+        <View style={{ position: "absolute" }}>
+          <Animated.View
+            {...this.panResponder.panHandlers}
+            style={[panStyle, styles.circle, {opacity:this.state.opacity}]}
+          />
+        </View>
+      );
+    }
+  }
+}
+
 class RecordingDetailsScreen extends React.Component {
   render() {
-    const { memory } = this.props.navigation.state;
+    const { params } = this.props.navigation.state;
+    const itemsPerRow = 3;
+    const data = params.goals
     return (
-      <View>
-      <Text> Memory Details View </Text>
+      <View style={{backgroundColor: '#C8E0E3', flex: 1, alignItems: 'center'}}>
+        <Text style={{fontSize:30, color: '#646363', fontFamily: 'Gill Sans'}}> {params.title} </Text>
+        <Text style={{fontSize:14, color: '#646363', fontFamily: 'Gill Sans'}}> {"November "}{params.date} </Text>
+        <Image style={{width: 150, height: 150}} source={{uri: 'https://i.ibb.co/D4Hwg4L/Screen-Shot-2018-11-30-at-2-33-39-AM.png'}}/>
+        <Text style={{textAlign: 'center', fontSize:14, color: '#646363', fontFamily: 'Gill Sans', paddingVertical: 20, paddingLeft: 20, paddingRight: 20}}> {"Good job with "}{(params.goals[0].goalTitle).toLowerCase()}{"! Here are some things we can continue working on: "} </Text>
+        <Text> {params.skill} </Text>
+        <View style={styles.container}>
+        <View style={styles.dropZone}>
+          <Text style={styles.text}>Drop them here!</Text>
+        </View>
+        <View style={styles.ballContainer} />
+        <Text> {params.goals[1].goalTitle} </Text>
+        <View style={styles.row}>
+          <Draggable />
+          <Draggable />
+          <Draggable />
+          <Draggable />
+          <Draggable />
+        </View>
+      </View>
+
       </View>
 
     );
   }
 }
 
-const styles = StyleSheet.create({
+let CIRCLE_RADIUS = 30;
 
+const styles = StyleSheet.create({
   logo: {
     width: 200,
     height: 100,
@@ -150,8 +246,35 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center',
-    color: 'white',
-    marginTop: 0
+  },
+    ballContainer: {
+    height:100,
+  },
+  circle: {
+    backgroundColor: "skyblue",
+    width: CIRCLE_RADIUS * 2,
+    height: CIRCLE_RADIUS * 2,
+    borderRadius: CIRCLE_RADIUS
+  },
+  row: {
+    flexDirection: "row"
+  },  
+  dropZone: {
+    height: 100,
+    backgroundColor: 'white'
+  },
+  text: {
+    marginTop: 25,
+    marginLeft: 5,
+    marginRight: 5,
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 25,
+    fontWeight: "bold"
+  },
+  list: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
   },
   buttonLeft: {
     backgroundColor: 'transparent',
@@ -231,6 +354,7 @@ goalMap.set(SKILLS[0], PublicSpeakingGoals);
 goalMap.set(SKILLS[1], MakingNewFriendsGoals);
 goalMap.set(SKILLS[2], TalkingToCrushGoals);
 
+
 const skillMap = new Map();
 var skill0Arr = [];
 skillMap.set(SKILLS[0], skill0Arr);
@@ -242,6 +366,11 @@ skillMap.set(SKILLS[2], skill2Arr);
 var MEMORIES = [];
 var count;
 
+var SKILL_OPTIONS = ["All"];
+for(count = 0; count < SKILLS.size; count++) {
+  SKILL_OPTIONS.push(SKILLS[count]);
+}
+
 for(count = 0; count < 10; count++) {
   var skillArr = [];
   var skillChosen = SKILLS[getRandomInt(goalMap.size)];
@@ -249,7 +378,11 @@ for(count = 0; count < 10; count++) {
   var skillArr = goalMap.get(skillChosen);
   var i;
   for(i = 0; i < 2; i++){
-    goalsArr.push(skillArr[getRandomInt(skillArr.length)]);
+    var elem = {
+      goalTitle: skillArr[getRandomInt(skillArr.length)],
+      index: i,
+    }
+    goalsArr.push(elem);
   };
   var mem = {
     index: count,
