a50e6b07a37becfffc10e5457c79c9302db2228d
sjiang06 <yuchenj@stanford.edu>
2018-12-04 12:26:01 -0800

    created achievements page

---
+++ bolder-app/AchievementsScreen.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import { SQLite } from 'expo';
+import styled from 'styled-components/native';
+import {Button, View, 
+  Text, StyleSheet, 
+  Image, ScrollView, 
+  TouchableOpacity, PanResponder,
+  Animated} from 'react-native';
+import {createStackNavigator, createAppContainer, createBottomTabNavigator} from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
+import styles from './src/stylesheet'
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+var SKILLS = ["Public Speaking", "Making New Friends", "Talking to Crush"];
+var PublicSpeakingGoals = ["Self Confidence", "Pacing", "Memorization", "Stuttering"];
+var MakingNewFriendsGoals = ["Asking Questions", "Introducing Self", "Initiating Hangouts", "Being Vulnerable"];
+var TalkingToCrushGoals = ["Texting first", "Planning hangouts", "Physical Affection", "Asking Questions"];
+const goalMap = new Map();
+goalMap.set(SKILLS[0], PublicSpeakingGoals);
+goalMap.set(SKILLS[1], MakingNewFriendsGoals);
+goalMap.set(SKILLS[2], TalkingToCrushGoals);
+
+
+const skillMap = new Map();
+var skill0Arr = [];
+skillMap.set(SKILLS[0], skill0Arr);
+var skill1Arr = [];
+skillMap.set(SKILLS[1], skill1Arr);
+var skill2Arr = [];
+skillMap.set(SKILLS[2], skill2Arr);
+
+var MEMORIES = [];
+var count;
+
+var SKILL_OPTIONS = ["All"];
+for(count = 0; count < SKILLS.size; count++) {
+  SKILL_OPTIONS.push(SKILLS[count]);
+}
+
+for(count = 0; count < 10; count++) {
+  var skillArr = [];
+  var skillChosen = SKILLS[getRandomInt(goalMap.size)];
+  var goalsArr = [];
+  var skillArr = goalMap.get(skillChosen);
+  var i;
+  for(i = 0; i < 2; i++){
+    var elem = {
+      goalTitle: skillArr[getRandomInt(skillArr.length)],
+      index: i,
+    }
+    goalsArr.push(elem);
+  };
+  var mem = {
+    index: count,
+    title: "PENDING",
+    date: getRandomInt(31),
+    skill: skillChosen,
+    goals: goalsArr,
+  };
+  skillMap.get(mem.skill).push(mem.index);
+  MEMORIES.push(mem);
+};
+
+function compareMemories(a, b){
+  return b.date>a.date
+}
+
+MEMORIES.sort(compareMemories)
+
+const ImageContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+`;
+
+
+export default class AchievementsScreen extends Component {
+	render() {
+		return(
+			<View style={{paddingLeft: 30}}>
+	        <Text style={{fontSize: 20, color: '#00B2CA', fontFamily: 'Gill Sans'}}>RECENT</Text>
+	        <TouchableOpacity>
+	          <View style={{flexDirection:'row'}}>
+	            <Image style={{width: 130, height: 130}} source={{uri: 'https://i.ibb.co/D4Hwg4L/Screen-Shot-2018-11-30-at-2-33-39-AM.png'}}/>                  
+	                <View style={{flexDirection:'column'}}>
+	                    <Text style={{fontSize:20, color: '#000000', fontFamily: 'Gill Sans'}}> {MEMORIES[0].skill} </Text>
+	                    <Text style={{fontSize:20, color: '#FBD1A2', paddingVertical: 10,fontFamily: 'Gill Sans'}}> {"November "}{MEMORIES[0].date} </Text>
+	                    <Text style={{fontSize:16, color: '#FE938C', paddingVertical: 10, fontFamily: 'Gill Sans'}}> {MEMORIES[0].goals[0].goalTitle} </Text>
+	                </View>
+	            </View>
+	        </TouchableOpacity>
+	        <Text style={{fontSize: 20, color: '#00B2CA', fontFamily: 'Gill Sans'}}>SHOW: </Text>
+	        <ScrollView vertical={true} fillViewPort="true" contentContainerStyle={styles.scrollstyle}>
+	          <ImageContainer>
+	            {MEMORIES.map((memory, index) => (
+	              <TouchableOpacity 
+	                onPress={() => this.props.navigation.navigate('RecordingDetails', memory)}
+	                key={index}>
+	                <View style={{flexDirection:'row'}}>
+	                  <Image source={{uri: 'https://i.ibb.co/D4Hwg4L/Screen-Shot-2018-11-30-at-2-33-39-AM.png'}} key ={index} />
+	                  <View style={{flexDirection:'column'}}>
+	                    <Text style={{fontSize:20, color: '#646363', fontFamily: 'Gill Sans'}}> {"November "}{memory.date} </Text>
+	                    <Text style={{fontSize:16, color: '#FBD1A2', paddingVertical: 0, fontFamily: 'Gill Sans'}}> {memory.skill} </Text>
+	                    <Text style={{fontSize:16, color: '#FE938C', paddingVertical: 0, fontFamily: 'Gill Sans'}}> {memory.goals[0].goalTitle} </Text>
+	                  </View>
+	                </View>
+	              </TouchableOpacity>
+	            ))}
+	          </ImageContainer>
+	        </ScrollView>
+	      </View>
+		);
+	}
+};
--- bolder-app/App.js
+++ bolder-app/App.js
@@ -8,8 +8,10 @@ import {Button, View,
   Animated} from 'react-native';
 import {createStackNavigator, createAppContainer, createBottomTabNavigator} from 'react-navigation';
 import { Ionicons } from '@expo/vector-icons';
+import AchievementsScreen from './AchievementsScreen.js';
+import styles from './src/stylesheet'
 
-export class HomeScreen extends React.Component {
+class HomeScreen extends React.Component {
   render() {
     const resizeMode = 'cover';
     return (
@@ -55,7 +57,7 @@ export class HomeScreen extends React.Component {
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={()=>{alert("Going to recordings...")}}>
+        <TouchableOpacity onPress={()=>this.props.navigation.navigate('AchievementList')}>
           <View>
             <Image style={styles.buttonRight} source={{uri: 'https://i.ibb.co/6D7f4kq/blue-blob.png'}} 
               resizeMode="contain"
@@ -247,114 +249,6 @@ class RecordingDetailsScreen extends React.Component {
   }
 }
 
-let CIRCLE_RADIUS = 30;
-
-const styles = StyleSheet.create({
-  logo: {
-    width: 200,
-    height: 100,
-    marginBottom: 0,
-    marginTop: 75,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-    ballContainer: {
-    height:100,
-  },
-  circle: {
-    backgroundColor: "skyblue",
-    width: CIRCLE_RADIUS * 2,
-    height: CIRCLE_RADIUS * 2,
-    borderRadius: CIRCLE_RADIUS
-  },
-  row: {
-    flexDirection: "row"
-  },  
-  dropZone: {
-    height: 100,
-    backgroundColor: 'white'
-  },
-  text: {
-    marginTop: 25,
-    marginLeft: 5,
-    marginRight: 5,
-    textAlign: "center",
-    color: "#fff",
-    fontSize: 25,
-    fontWeight: "bold"
-  },
-  list: {
-    flexDirection: 'row',
-    flexWrap: 'wrap'
-  },
-  buttonLeft: {
-    backgroundColor: 'transparent',
-    flex: 1,
-    width: 180,
-    height: 180,
-    position: 'absolute',
-    top: -60,
-    left: -180
-  },
-  buttonRight: {
-    backgroundColor: 'transparent',
-    flex: 1,
-    width: 180,
-    height: 180,
-    position: 'absolute',
-    top: -60,
-    right: -180
-  },
-  iconRight: {
-    backgroundColor: 'transparent',
-    flex: 1,
-    width: 120,
-    height: 120,
-    position: 'absolute',
-    top: -30,
-    right: -150
-  },
-  iconLeft: {
-    backgroundColor: 'transparent',
-    flex: 1,
-    width: 120,
-    height: 120,
-    position: 'absolute',
-    top: -30,
-    left: -150
-  },
-  textLeft: {
-    position: 'absolute',
-    top: 110,
-    right: -180,
-    fontFamily: 'Gill Sans',
-    fontSize: 16,
-    color: 'white'
-  },
-  textRight: {
-    position: 'absolute',
-    top: 110,
-    left: -180,
-    fontFamily: 'Gill Sans',
-    fontSize: 16,
-    color: 'white'
-
-  },
-  iconSmall: {
-    width: 100,
-    height: 100,
-  },
-  scrollstyle: {
-    marginBottom:30
-  },
-  font: {
-    fontFamily: 'Gill Sans'
-  }
-});
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -416,11 +310,14 @@ function compareMemories(a, b){
 
 MEMORIES.sort(compareMemories)
 
+/*Navigation for app*/
+
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
     Recordings: RecordingsScreen,
     RecordingDetails: RecordingDetailsScreen,
+    AchievementList: AchievementsScreen,
   },
   {
     initialRouteName: 'Home',
