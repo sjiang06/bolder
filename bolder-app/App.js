51921885da2b7062587836ff38850ed3a7b0f8bd
sjiang06 <yuchenj@stanford.edu>
2018-12-06 01:29:08 -0800

    removed extra homescreen file

--- bolder-app/App.js
+++ bolder-app/App.js
@@ -16,8 +16,8 @@ import HeadphoneScreen from './HeadphoneScreen.js';
 import SyncScreen from './SyncScreen.js';
 import HelpScreen from './HelpScreen.js';
 import styles from './src/stylesheet';
-import HomeScreen from './HomeScreen.js';
 import NewRecordingScreen from './NewRecordingScreen.js';
+import SelectDeviceScreen from './SelectDeviceScreen.js';
 
 const ImageContainer = styled.View`
   display: flex;
@@ -36,6 +36,82 @@ var colorMap = new Map([
   ]);
 
 var SKILLS = ["Public Speaking", "Making New Friends", "Talking to Crush"];
+
+class HomeScreen extends React.Component {
+  render() {
+    const resizeMode = 'cover';
+    return (
+      <View style={{ flex: 1}}>
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <Image
+            style={{
+              flex: 1,
+              resizeMode,
+            }}
+            source={{uri: 'https://i.ibb.co/kyynhkK/home.png'}}
+          />
+        </View>
+
+
+         
+         <View style={styles.logo}>
+          <Image source={{uri: 'https://i.ibb.co/QXQkQmB/BOLDERlogo.png'}} 
+          style={{width: 350, height: 200, alignItems: 'center'}}/>
+        </View>
+        <View style={styles.container}>
+        
+        <TouchableOpacity onPress={() => {
+            /* 1. Navigate to the Recordings route with params */
+            this.props.navigation.navigate('Recordings');
+          }}>
+          <View style={styles.columnView}>
+            <Image style={styles.buttonLeft} source={{uri: 'https://i.ibb.co/6D7f4kq/blue-blob.png'}} 
+              resizeMode="contain"
+           />
+            <Image style={styles.iconLeft} source={{uri: 'https://i.ibb.co/RHhhSgB/noun-Playlist-972339.png'}} 
+              resizeMode="contain"
+            />
+            <Text style={styles.textRight}> RECORDINGS </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={()=>this.props.navigation.navigate('AchievementList')}>
+          <View>
+            <Image style={styles.buttonRight} source={{uri: 'https://i.ibb.co/6D7f4kq/blue-blob.png'}} 
+              resizeMode="contain"
+            />
+            <Image style={styles.iconRight} source={{uri: 'https://i.ibb.co/SJzVtRf/achievements.png'}} 
+              resizeMode="contain"
+            />
+            <Text style={styles.textLeft}> ACHIEVEMENTS </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>this.props.navigation.navigate('PracticeScreen', {title:MEMORIES[0].title, date: MEMORIES[0].date, params:{
+                  skill: MEMORIES[0].skill,
+                  goalChosen: MEMORIES[0].goal,
+                  color: colorMap.get(MEMORIES[0].skill),
+                }})}>
+          <View style={style={paddingTop: 3, width: 300, height: 150, alignItems: 'center'}}>
+            <View style={{backgroundColor:'#B3B3B3', width: 250, height: 130, alignItems: 'center', borderRadius:25, flexDirection:'row', justifyContent:'flex-start', top: 150, borderWidth:3, borderColor: 'white'}}>
+              <Image style={{width: 100, height: 100, marginRight: 20}} source={{uri: 'https://i.ibb.co/CMWPKFt/rock.png'}}/>
+              <Text style={{fontSize:20, width: 125, textAlign: 'left', color: 'white', fontFamily: 'Gill Sans'}}>One recording recently uploaded! </Text>
+            </View>
+          </View>
+      </TouchableOpacity>
+
+      </View>
+      </View>
+    );
+  }
+}
 
 class RecordingsScreen extends React.Component {
   constructor() {
@@ -217,7 +293,8 @@ const RootStack = createStackNavigator(
     PracticeScreen: PracticeScreen,
     HeadphoneScreen: HeadphoneScreen,
     SyncScreen: SyncScreen,
-    NewRecordingScreen: NewRecordingScreen
+    NewRecordingScreen: NewRecordingScreen,
+    DeviceScreen: SelectDeviceScreen
   },
   {
     initialRouteName: 'HomeScreen',
--- bolder-app/PracticeScreen.js
+++ bolder-app/PracticeScreen.js
@@ -28,12 +28,6 @@ export default class PracticeScreen extends Component {
 	    return (
 	    	<View style={{backgroundColor: '#C8E0E3', flex: 1, alignItems: 'center'}}>
 
-	    			<TextInput
-			          style={{backgroundColor: '#C8E0E3', flex: 1, alignItems: 'center'}}
-			          onChangeText={(text) => this.title.setState({text})}
-			          value={this.state.title}
-			        />
-
 			        <Text style={{fontSize:30, color: '#646363', fontFamily: 'Gill Sans', marginTop:50}}> {params.title} </Text>
 			        <Text style={{fontSize:20, color: '#646363', fontFamily: 'Gill Sans'}}>October {params.date}</Text>
 			        <Image style={{width: 150, height: 150, margin: 20}} source={{uri: 'https://i.ibb.co/D4Hwg4L/Screen-Shot-2018-11-30-at-2-33-39-AM.png'}}/>
