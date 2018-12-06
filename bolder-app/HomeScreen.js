05c4c40fc6eae4d8f53600dfd4fbc571cb5b9d21
Sofía Dudas <sdudas@stanford.edu>
2018-12-05 21:46:03 -0800

    should load now

--- bolder-app/HomeScreen.js
+++ bolder-app/HomeScreen.js
@@ -14,6 +14,7 @@ import styles from './src/stylesheet';
 
 export default class HomeScreen extends React.Component {
   constructor() {
+  	super();
   	var chosenSkill = SKILLS[getRandomInt(SKILLS.length)];
   }
 
